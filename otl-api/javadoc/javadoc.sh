e84f1697ab9dc15df4a47048ab45d9e02d480b2f
Sudiptasish Chanda <sudiptasish.chanda@aexp.com>
2020-04-03 23:49:23 +0530

    Adding javadoc

---
+++ otl-api/javadoc/javadoc.sh
@@ -0,0 +1,1 @@
+/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/../bin/javadoc -J-Dhttp.proxyHost=proxy-phoenix.aexp.com -J-Dhttp.proxyPort=8080 -J-Dhttps.proxyHost=proxy-phoenix.aexp.com -J-Dhttps.proxyPort=8080 "-J-Dhttp.nonProxyHosts=\"*localhost*^|*ci-repo.aexp.com*\"" @options @packages
